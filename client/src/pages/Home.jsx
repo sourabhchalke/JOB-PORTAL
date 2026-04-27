02903169d63c5847595240895f6a779f96ba692e
Sourabh Chalke <sourabhchalke2157@gmail.com>
2026-04-27 11:17:31 +0530

    JobListing component created and Current Search sidebar added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
+import JobListing from '../components/JobListing'
 
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
     <div>
       <Navbar/>
       <Hero/>
+      <JobListing/>
     </div>
   )
 }
