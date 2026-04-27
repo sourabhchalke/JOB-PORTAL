f77b2ae8660f0e28233c2ff0642a64ee5b3552f7
Sourabh Chalke <sourabhchalke2157@gmail.com>
2026-04-27 17:32:59 +0530

    Pagination added for jobs card at the end

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -8,6 +8,8 @@ const JobListing = () => {
 
   const [showFilter,setShowFilter] = useState(false);
 
+  const [currentPage,setCurrentPage]=useState(1);
+
   return (
     <div className="container 2xl:px-20 mx-auto flex flex-col lg:flex-row max-lg:space-y-8 py-8">
       {/* SideBar */}
@@ -86,10 +88,28 @@ const JobListing = () => {
                 <h3 className="font-medium text-3xl py-2" id="job-list">Latest Jobs</h3>
                 <p className="mb-8">Get your desired job from top companies</p>
                 <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4">
-            {jobs.map((job,index)=>(
+            {jobs.slice((currentPage-1)*6,currentPage*6).map((job,index)=>(
                 <JobCard key={index} job={job}/>
             ))}
                 </div>
+
+            {/* Pagination */}
+          {jobs.length>0 && (
+            <div className="flex items-center justify-center space-x-2 mt-10">
+              <a href="#job-list">
+                <img onClick={()=>setCurrentPage(Math.max(currentPage-1),1)} src={assets.left_arrow_icon} alt="" />
+              </a>
+              {Array.from({length:Math.ceil(jobs.length/6)}).map((_,index)=>(
+                <a href="#job-list">
+                  <button onClick={()=>setCurrentPage(index+1)} className={`cursor-pointer w-10 h-10 flex items-center justify-center border border-gray-300 rounded ${currentPage === index+1 ? 'bg-blue-100 text-blue-500' : 'text-gray-500'}`}>{index+1}</button>
+                </a>
+              ))}
+              <a href="#job-list">
+                <img onClick={()=>setCurrentPage(Math.min(currentPage+1,Math.ceil(jobs.length/6)))} src={assets.right_arrow_icon} alt="" />
+              </a>
+            </div>
+          )}
+
             </section>
 
     </div>
