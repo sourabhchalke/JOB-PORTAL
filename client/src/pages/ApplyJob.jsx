89a1b1c800303d69ce00867bca04e16dc78856e6
Sourabh Chalke <sourabhchalke2157@gmail.com>
2026-05-04 14:41:25 +0530

    Getting Apply Job data using 'id' through params

--- client/src/pages/ApplyJob.jsx
+++ client/src/pages/ApplyJob.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+
+import { useParams } from "react-router-dom";
+
+import { AppContext } from "../context/AppContext";
 
 const ApplyJob = () => {
-  return (
-    <div>
-      <h2>ApplyJob</h2>
-    </div>
-  )
-}
+  const { id } = useParams();
 
-export default ApplyJob
+  const [jobData, setJobData] = useState(null);
+
+  const { jobs } = useContext(AppContext);
+
+  const fetchJob = async () => {
+    const data = jobs.filter(job => job._id === id);
+    if (data.length !== 0) {
+      setJobData(data[0]);
+      console.log(data[0]);
+    }
+  };
+
+  useEffect(() => {
+    if ((jobs.length > 0)) {
+      fetchJob();
+     }
+  }, [id, jobs]);
+
+  return <div></div>;
+};
+
+export default ApplyJob;
