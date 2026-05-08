07c7cd50f54927c633486786d691c11c23faa8fe
Sourabh Chalke <sourabhchalke2157@gmail.com>
2026-05-08 16:34:38 +0530

    Dashboard's Left Side created which shows add job,manage job and view applications buttons

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Outlet, useNavigate } from "react-router-dom";
+import { NavLink, Outlet, useNavigate } from "react-router-dom";
 import { assets } from "../assets/assets";
 
 const Dashboard = () => {
@@ -26,7 +26,32 @@ const Dashboard = () => {
         </div>
       </div>
 
-      {/* <Outlet/> */}
+    <div className="flex items-start">
+        {/* Left Sidebar with option to add job,manage jobs,view applications */}
+        <div className="inline-block min-h-screen border-r-2">
+            <ul className="flex flex-col items-start pt-5 text-gray-800">
+                <NavLink className={({isActive})=>`flex items-center p-3 sm:px-6 gap-2 w-full hover:bg-gray-100 ${isActive && 'bg-blue-100 border-r-4 border-blue-500'}`} to={'/dashboard/add-job'}>
+                    <img className="min-w-4" src={assets.add_icon} alt="" />
+                    <p className="max-sm:hidden">Add Job</p>
+                </NavLink>
+                <NavLink className={({isActive})=>`flex items-center p-3 sm:px-6 gap-2 w-full hover:bg-gray-100 ${isActive && 'bg-blue-100 border-r-4 border-blue-500'}`} to={'/dashboard/manage-jobs'}>
+                    <img className="min-w-4" src={assets.home_icon} alt="" />
+                    <p className="max-sm:hidden">Manage Job</p>
+                </NavLink>
+                <NavLink className={({isActive})=>`flex items-center p-3 sm:px-6 gap-2 w-full hover:bg-gray-100 ${isActive && 'bg-blue-100 border-r-4 border-blue-500'}`} to={'/dashboard/view-applications'}>
+                    <img className="min-w-4" src={assets.person_tick_icon} alt="" />
+                    <p className="max-sm:hidden">View Applications</p>
+                </NavLink>
+            </ul>
+        </div>
+
+        <div>
+            <Outlet/>
+        </div>
+
+    </div>
+
+      
     </div>
   );
 };
