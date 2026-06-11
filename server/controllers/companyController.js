21c983e231d176aa1933d0928f763f0daaa44ecc
Sourabh Chalke <sourabhchalke2157@gmail.com>
2026-06-11 18:39:37 +0530

    postJon created where we post new jobs

--- server/controllers/companyController.js
+++ server/controllers/companyController.js
@@ -103,6 +103,7 @@ import bcrypt from "bcrypt";
 import { v2 as cloudinary } from "cloudinary";
 import generateToken from "../utils/generateToken.js";
 import fs from "fs"; // For deleting temp file after upload
+import Job from '../models/Job.js';
 
 // Register a new company
 export const registerCompany = async (req, res) => {
@@ -240,8 +241,32 @@ export const getCompanyData = async (req, res) => {
 // Post a new job
 export const postJob = async (req, res) => {
   
-  const {title,description,location,salary}=req.body;
+  const {title,description,location,salary,level,category}=req.body;
 
+  const companyId = req.company._id;
+
+  // console.log(companyId,{title,description,location,salary});
+
+  try {
+    
+    const newJob = new Job({
+      title,
+      description,
+      location,
+      salary,
+      companyId,
+      date:Date.now(),
+      level,
+      category
+    })
+
+    await newJob.save();
+
+    res.json({success:true,newJob});
+
+  } catch (error) {
+    res.json({success:false,message:error.message});
+  }
 
 };
 
