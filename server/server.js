34ce97efd45d0277931a6d5169263d8b8603f942
Sourabh Chalke <sourabhchalke2157@gmail.com>
2026-06-11 19:13:22 +0530

    Created jobRoutes,jobController and jobSchema which is use for getting jobs

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ import * as Sentry from '@sentry/node';
 import { clerkWebhooks } from './controllers/webhooks.js';
 import companyRoutes from './routes/companyRoutes.js';
 import connectCloudinary from './config/cloudinary.js';
+import jobRoutes from './routes/jobRoutes.js';
 
 // Initialize Express
 const app = express();
@@ -39,6 +40,7 @@ app.post('/test', (req, res) => {
 });
 
 app.use('/api/company',companyRoutes);
+app.use('/api/jobs',jobRoutes);
 
 
 // PORT
