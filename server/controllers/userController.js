e2b941ba4b970bbd843bc618407ad5ca4969d57a
Sourabh Chalke <sourabhchalke2157@gmail.com>
2026-06-14 19:05:38 +0530

    clerkMiddleware added and getUserData function of userController updated

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,23 @@
+import User from '../models/User.js';
 
 //Get User data
 export const getUserData = async(req,res)=>{
+
+    const userId = req.auth.userId;
+
+    try {
+        
+        const user = await User.findById(userId);
+
+        if(!user){
+            return res.json({success:false,message:"User Not Found"});
+        }
+
+        res.json({success:true,user});
+
+    } catch (error) {
+        res.json({success:false,message:error.message});
+    }
 
 }
 
@@ -16,5 +33,5 @@ export const getUserJobApplications=async(req,res)=>{
 
 //Update user profile(resume)
 export const updateUserResume = async(req,res)=>{
-    
+
 }
--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ import companyRoutes from './routes/companyRoutes.js';
 import connectCloudinary from './config/cloudinary.js';
 import jobRoutes from './routes/jobRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import {clerkMiddleware} from '@clerk/express';
 
 // Initialize Express
 const app = express();
@@ -27,6 +28,7 @@ app.post(
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(clerkMiddleware());
 
 // Routes
 app.get('/',(req,res)=>res.send("API Working"))
