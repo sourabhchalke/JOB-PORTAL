2cb620d50cbeec2687ef8136e83f2048c5085011
Sourabh Chalke <sourabhchalke2157@gmail.com>
2026-06-16 17:42:58 +0530

    Axios added for add job in AddJob page

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -1,6 +1,10 @@
 import React, { useState,useRef, useEffect } from 'react'
 import Quill from 'quill';
 import { JobCategories, JobLocations } from '../assets/assets';
+import { useContext } from 'react';
+import {AppContext} from '../context/AppContext';
+import { toast } from 'react-toastify';
+import axios from 'axios';
 
 const AddJob = () => {
 
@@ -12,6 +16,34 @@ const AddJob = () => {
 
     const editorRef = useRef(null);
     const quillRef = useRef(null);
+
+    const {backendUrl,companyToken} = useContext(AppContext);
+
+    const onSubmitHandler = async(e)=>{
+        e.preventDefault()
+
+        try {
+            
+            const description = quillRef.current.root.innerHTML;
+
+            const {data} = await axios.post(backendUrl+'/api/company/post-job',
+                {title,description,location,salary,category,level},
+                {headers:{token:companyToken}}
+            )
+
+            if(data.success){
+                toast.success(data.message);
+                setTitle('')
+                setSalary(0)
+                quillRef.current.root.innerHTML=""
+            }else{
+                toast.error(data.message);
+            }
+
+        } catch (error) {
+            toast.error(error.message);
+        }
+    }
 
 
     useEffect(()=>{
@@ -25,7 +57,7 @@ const AddJob = () => {
 
   return (
     
-    <form className='container flex flex-col p-4 md:p-10 w-full items-start gap-3' action="">
+    <form onSubmit={onSubmitHandler} className='container flex flex-col p-4 md:p-10 w-full items-start gap-3' action="">
         <div className='w-full'>
             <p className='mb-2'>Job Title</p>
             <input type="text" placeholder='Type Here' onChange={e=>setTitle(e.target.value)} value={title} required className='w-full max-w-lg px-3 py-2 border-2 border-gray-300 rounded'/>
